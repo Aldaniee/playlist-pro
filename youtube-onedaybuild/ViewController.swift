5552974b55e8913c53ce702622639cd6a057e639
Aidan Lee <aidanwlee@gmail.com>
2020-11-01 14:53:00 -0500

    lesson 5

--- youtube-onedaybuild/ViewController.swift
+++ youtube-onedaybuild/ViewController.swift
@@ -7,17 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate, ModelDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     var model = Model()
+    var videos = [Video]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set itself as the datasource and the delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        model.delegate = self
         
         model.getVideos()
     }
-
+    
+    // MARK: – Model Delegate Methods
+    func videosFetched(_ videos: [Video]) {
+        self.videos = videos
+        tableView.reloadData()
+    }
+    
+    // MARK: – TableView Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        // Configure the cell with the data
+        let title = self.videos[indexPath.row].title
+        
+        cell.textLabel?.text = title
+        // Return the cell
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 
 }
 
