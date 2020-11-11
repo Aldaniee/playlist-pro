91770ef814cec202a668e4b6b7db585854978631
Aidan Lee <aidanwlee@gmail.com>
2020-11-11 11:45:03 -0500

    init

--- youtube-onedaybuild/Helpers/Constants.swift
+++ youtube-onedaybuild/Helpers/Constants.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 struct Constants {
-    static var API_KEY = "***REMOVED***"
-    static var PLAYLIST_ID = "UUnxQ8o9RpqxGF2oLHcCn9VQ"
-    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-    static var VIDEOCELL_ID = "VideoCell"
-    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
+    static let API_KEY = "***REMOVED***"
+    static let PLAYLIST_ID = "UUnxQ8o9RpqxGF2oLHcCn9VQ"
+    static let API_PLAYBACK_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static let VIDEOCELL_ID = "VideoCell"
+    static let YT_EMBED_URL = "https://www.youtube.com/embed/"
+    static let MAX_RESULTS = 10
+    //static let API_SEARCH_URL =  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=\(SearchEngine.MAX_RESULTS)&order=relevance&q=\(text)&type=video&key=\(SearchEngine.API_KEY)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
 }
